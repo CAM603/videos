e6049fec10f3c243dfecbf38e1b75668f059cf2f
CAM603 <cameronhawley@comcast.net>
2019-12-31 11:01:32 -0500

    added search function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
 
     onFormSubmit = (event) => {
         event.preventDefault();
-
+        this.props.onTermSubmit(this.state.term)
     };
     render() {
         return (
